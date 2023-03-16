b9d90e4c2110d570a5cad5537fb850e56ad98164
Bryan Marino <bryan-marino0312@outlook.com>
2023-03-16 08:34:17 -0500

    post endpoints done

--- index.js
+++ index.js
@@ -7,22 +7,28 @@ const bodyParser = require("body-parser");
 //Mongoose Set up
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const userSchema = new Schema(
-  {
-    username: { type: String, required: true },
-  },
-  { versionKey: false }
-);
-const User = mongoose.model("User", userSchema);
+
 const exerciseSchema = new Schema(
   {
-    user: { type: Schema.Types.ObjectId, ref: User },
+    // user: { type: Schema.Types.ObjectId, ref: "User" },
     description: { type: String, required: true },
     duration: { type: Number, required: true },
     date: { type: Date, get: (date) => date.toDateString() },
   },
-  { versionKey: false, toJSON: { getters: true } }
+  { versionKey: false, toJSON: { getters: true, virtuals: true }, id: false }
 );
+exerciseSchema.options.toJSON.transform = function (doc, ret, options) {
+  delete ret._id;
+  return ret;
+};
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    exercises: [exerciseSchema],
+  },
+  { versionKey: false }
+);
+const User = mongoose.model("User", userSchema);
 const Exercise = mongoose.model("Exercise", exerciseSchema);
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -39,32 +45,51 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/users", async function (req, res) {
-  const users = await User.find();
+  const users = await User.find().select({ exercises: 0 });
   return res.json(users);
 });
 
 app.post("/api/users", async function (req, res) {
-  const { username } = req.body;
-  const newUser = new User({ username });
-  return res.json(await newUser.save());
+  const { username: _username } = req.body;
+  const newUser = new User({ username: _username });
+  const { _id, username } = await newUser.save();
+  return res.json({ _id, username });
 });
 
 app.post("/api/users/:_id/exercises", async function (req, res) {
   const { _id, description, duration, date } = req.body;
-  console.log(date);
-  console.log();
   const newExercise = new Exercise({
-    user: _id,
     description,
     duration,
     date: date ? date : Date.now(),
   });
-  console.log(newExercise);
-  return res.json(await newExercise.save());
+  const user = await User.findByIdAndUpdate(
+    _id,
+    {
+      $push: {
+        exercises: newExercise,
+      },
+    },
+    { new: true }
+  );
+  return res.json({
+    _id: user._id,
+    username: user.username,
+    ...user.exercises[0].toJSON(),
+  });
 });
 
 app.get("/api/users/:_id/logs", async function (req, res) {
-  return res.json(await Exercise.find());
+  const { from, to, limit } = req.query;
+
+  return res.json(
+    await User.findById(_id, {})
+    // await User.find()
+  );
+});
+
+app.delete("/api/exercises", async function (req, res) {
+  return res.json(await Exercise.deleteMany());
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
